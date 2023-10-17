466f7805e74e27e63c62656759454267e38687eb
[Renato <[renato.sistemas02@gmail.com]>
2023-10-16 20:08:31 -0500

    Implementacion de la automatizacion de login de acceso con credenciales incorrectos

--- cypress/e2e/login-swag-labs/login.cy.js
+++ cypress/e2e/login-swag-labs/login.cy.js
@@ -8,4 +8,11 @@ describe("The Login page",()=>{
         cy.get("#login-button").should("have.value","Login").click()
         cy.get(".app_logo").should("have.text","Swag Labs")
     })
+
+    it("Incorrect Credentials Error Message", ()=> {
+        cy.get("#user-name").type("renato").should("have.value", "renato")
+        cy.get("#password").type("password123")
+        cy.get("#login-button").should("have.value","Login").click()
+        cy.get().should("have.value","Epic sadface: Username and password do not match any user in this service")
+    })
 })
