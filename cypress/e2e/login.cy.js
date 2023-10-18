b1570dac1139d35af0725203eb026e49e58ffcf4
[Renato <[renato.sistemas02@gmail.com]>
2023-10-18 15:47:59 -0500

    Se termino de configurar el package.json

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,11 +1,13 @@
 
 describe("The Login page", () => {
   beforeEach(() => {
+
     cy.visit("https://www.saucedemo.com/");
     cy.get(".login_logo").should("be.visible");
+  
   });
 
-  it("User blocked", () => {
-    cy.loginData();
+  it("Login Successfull", () => {
+    cy.loginCredentials();
   });
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,19 +1,19 @@
-Cypress.Commands.add('login',({username,password})=>{
-       cy.get("#user-name")
-      .should("have.attr", "placeholder", "Username")
-      .type(username)
-      .should("have.value", username);
-    cy.get("#password")
-      .should("have.attr", "placeholder", "Password")
-      .type(password);
-      cy.get("#login-button").should("have.value", "Login").click();
-})
+Cypress.Commands.add("login", ({ username, password }) => {
+  cy.get("#user-name")
+    .should("have.attr", "placeholder", "Username")
+    .type(username)
+    .should("have.value", username);
+  cy.get("#password")
+    .should("have.attr", "placeholder", "Password")
+    .type(password);
+  cy.get("#login-button").should("have.value", "Login").click();
+});
 
-Cypress.Commands.add('loginData',()=>{
-    cy.fixture("loginData").then((data) => {
-        const { usernameSuccessfull, passwordSuccessfull } = data;
-        cy.get("#user-name").type(`${usernameSuccessfull}`); 
-        cy.get("#password").type(`${passwordSuccessfull}`); 
-        cy.get("#login-button").click();
-      });
-})
+Cypress.Commands.add("loginCredentials", () => {
+  cy.fixture("loginData").then((data) => {
+    const { usernameSuccessfull, passwordSuccessfull } = data;
+    cy.get("#user-name").type(`${usernameSuccessfull}`);
+    cy.get("#password").type(`${passwordSuccessfull}`);
+    cy.get("#login-button").click();
+  });
+});
